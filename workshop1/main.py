babd69f32ddf9eae5bc73f23ed0e691264bc0c19
Zuzanna Witczak <zuzannawitczak01@gmail.com>
2022-05-15 00:59:54 +0200

    finish

--- workshop1/main.py
+++ workshop1/main.py
@@ -1,37 +1,127 @@
+import datetime
 from typing import Dict
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from pydantic import BaseModel
 
 
 app = FastAPI()
+days = {"monday": 1, "tuesday": 2, "wednesday":3, "thursday":4, "friday":5, "saturday":6, "sunday": 7}
+events = dict()
 
+def validate_date(date):
+        try:
+            datetime.datetime.strptime(date, '%Y-%m-%d')
+        except ValueError:
+            return False
+        return True
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"start": "1970-01-01"}
+
+class GiveMethodResp(BaseModel):
+    method: str
+
+# class GiveMeSomethingResp(BaseModel):
+#     received: Dict
+#     constant_data: str = "python jest super"
+
+@app.get("/method", status_code=200, response_model=GiveMethodResp)
+def return_get():
+    return {"method": "GET"}
+
+@app.delete("/method", status_code=200, response_model=GiveMethodResp)
+def return_delete():
+    return GiveMethodResp(method="DELETE", status_code=200, response_model=GiveMethodResp)
+
+@app.options("/method", status_code=200, response_model=GiveMethodResp)
+def return_options():
+    return GiveMethodResp(method="OPTIONS")
+
+@app.put("/method", status_code=200, response_model=GiveMethodResp)
+def return_put():
+    return GiveMethodResp(method="PUT")
+
+@app.post("/method", status_code=201, response_model=GiveMethodResp)
+def return_post():
+    return GiveMethodResp(method="POST")
+
+@app.get("/day", status_code=200)
+def check_name_number(name: str, number: int):
+    if name in days.keys():
+        if days[name] == number:
+            return {"day":f"{name}"}
+    raise HTTPException(status_code=400, detail="Day and number are not matching")
+    
+
+class NewEventRq(BaseModel):
+    date: str
+    event: str
+
+
+class NewEventResp(BaseModel):
+    id: int
+    name: str
+    date: str
+    date_added: str
+
+@app.put("/events", response_model=NewEventResp)
+def create_event(new_event: NewEventRq):
+    new_event.date 
+    global events
+    if len(events.keys()) == 0:
+        ind = 0
+    else:
+        ind = max(events.keys()) + 1
+        
+    new_date_added = datetime.date.today().isoformat()
+    
+    events.update({ind:{"name": new_event.event, "date": new_event.date, "date_added": new_date_added}})
+    
+    return NewEventResp(id = ind, name = new_event.event, date=new_event.date, date_added=new_date_added)
+    
+
+
+@app.get("/events/{date}", status_code=200)
+def return_events(date: str):
+    if validate_date(date) == True:
+        returned_events = []
+        counter = 0
+        for key, val in events.items():
+            if val['date'] == date:
+                returned_events.append({'id': key, 'name':val['name'], 'date':date, 'date_added':val['date_added']})
+                counter += 1
+        if counter != 0:
+            return tuple(returned_events)
+        else:
+            raise HTTPException(status_code=404, detail="No events")
+
+    else:
+        raise HTTPException(status_code=400, detail="Invalid data")
+
+    # return tuple(returned_events)
 
 
 class HelloResp(BaseModel):
     msg: str
 
 
-@app.get("/hello/{name}", response_model=HelloResp)
-def read_item(name: str):
-    return HelloResp(msg=f"Hello {name}")
+# @app.get("/hello/{name}", response_model=HelloResp)
+# def read_item(name: str):
+#     return HelloResp(msg=f"Hello {name}")
+
+# class GiveMeSomethingRq(BaseModel):
+#     first_key: str
 
 
-class GiveMeSomethingRq(BaseModel):
-    first_key: str
+# class GiveMeSomethingResp(BaseModel):
+#     received: Dict
+#     constant_data: str = "python jest super"
 
 
-class GiveMeSomethingResp(BaseModel):
-    received: Dict
-    constant_data: str = "python jest super"
-
-
-@app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
-def receive_something(rq: GiveMeSomethingRq):
-    return GiveMeSomethingResp(received=rq.dict())
+# @app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
+# def receive_something(rq: GiveMeSomethingRq):
+#     return GiveMeSomethingResp(received=rq.dict())
 
--- workshop1/test_main.py
+++ workshop1/test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 import pytest
+import datetime
 from main import app
 
 client = TestClient(app)
@@ -7,17 +8,50 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"start": "1970-01-01"}
 
-@pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
-def test_hello_name(name):
-    # name = 'elo'
-    response = client.get(f"/hello/{name}")
-    assert response.status_code == 200
-    assert response.json() == {'msg': f"Hello {name}"}
+# @pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
+# def test_hello_name(name):
+#     # name = 'elo'
+#     response = client.get(f"/hello/{name}")
+#     assert response.status_code == 200
+#     assert response.json() == {'msg': f"Hello {name}"}
 
 
 def test_receive_something():
-    response = client.post("/dej/mi/coś", json={'first_key': 'some_value'})
-    assert response.json() == {"received": {'first_key': 'some_value'},
-                             "constant_data": "python jest super"}
+    response = client.put("/events", json={'date': '2022-02-02', 'event':'mamma'})
+    assert response.json() == {'id': 0,
+                                'name': 'mamma',
+                                'date': '2022-02-02',
+                                'date_added': datetime.date.today().isoformat()}
+
+def test_receive_something2():
+    response = client.put("/events", json={'date': '2022-02-02', 'event':'pappa'})
+    assert response.json() == {'id': 1,
+                                'name': 'pappa',
+                                'date': '2022-02-02',
+                                'date_added': datetime.date.today().isoformat()}
+
+def test_read():
+    response = client.get("/events/2022-02-02")
+    assert response.status_code == 200
+    assert response.json() == [{'id': 0,
+                                'name': 'mamma',
+                                'date': '2022-02-02',
+                                'date_added': datetime.date.today().isoformat()},
+                                {'id': 1,
+                                'name': 'pappa',
+                                'date': '2022-02-02',
+                                'date_added': datetime.date.today().isoformat()}]
+                                
+def test_no_events():
+    response = client.get("/events/2023-02-02")
+    assert response.status_code == 400
+    
+
+def test_receive_something3():
+    response = client.put("/events", json={"date": "2022-03-22", "event": "Drugi dzień wiosny"})
+
+def test_read_newdata():
+    response = client.get("/events/2022-0309-22")
+    assert response.status_code == 404
